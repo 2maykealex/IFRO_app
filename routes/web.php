680540277a621d457edc7032403aac77fe4c4729
Mayke <mayke.suporte@gmail.com>
2018-04-22 17:17:05 -0400

    Acrescentado algumas rotas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,18 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function () {
+
+    $this->get('area', 'CourseController@areaCurso')->name('admin.area.curso');    
+
+    $this->post('activity-store', 'ActivityController@activityStore')->name('admin.activity.store');    
+    $this->get('activity-new', 'ActivityController@newActivity')->name('admin.activity.new');    
+    $this->get('activities', 'ActivityController@activities')->name('admin.activities');    
+    $this->get('activity', 'ActivityController@index')->name('admin.activity');    
+    $this->get('/', 'AdminController@index')->name('admin.home');    
+
 });
+
+
+$this->get('/', 'Site\SiteController@index');
+
+Auth::routes();

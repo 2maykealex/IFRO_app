498460ee1055d35a2f2cfa34c0fc84cc5d899efb
Mayke <mayke.suporte@gmail.com>
2018-04-22 22:27:05 -0400

    Criado Models

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Course;
 
 class Area extends Model
 {
     public $timestamps = false;
 
-    
+    public function courses(){
+        return $this->hasMany(Course::class);
+    }
 }
--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Area;
 
 class Course extends Model
 {
     public $timestamps = false;
 
-    
+    public function area($id){
+        // return $this->hasOne(Area::class);
+        return $this->where('id', $id);
+    }
 }

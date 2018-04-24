17e2d00c51d942177b0cfebb3c4cfb24ea6737b6
Mayke <mayke.suporte@gmail.com>
2018-04-24 16:28:41 -0400

    várias

--- app/Http/Controllers/Admin/ActivityController.php
+++ app/Http/Controllers/Admin/ActivityController.php
@@ -4,8 +4,31 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Activity;
 
 class ActivityController extends Controller
 {
-    //
+    public function newActivity(){
+        return view('admin.activity.new');
+    }
+
+    public function activities(){
+        $activities = Activity::all();   
+        return view('admin.activity.activities', compact('activities'));
+    }
+
+    public function activityStore(Request $request, Activity $activities){
+        
+        $dataForm = $request->all();
+        
+        $response = $activities->activityNew($dataForm);
+
+        if ($response['success'])
+            return redirect()->route('admin.activities')
+                             ->with('success', $response['message']);
+
+        return redirect()->back()
+                         ->with('error', $response['message']);
+        
+    }
 }
--- config/adminlte.php
+++ config/adminlte.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 2',
+    'title' => 'IFRO_Apps',
 
     'title_prefix' => '',
 
@@ -30,9 +30,9 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>IFRO</b>Apps',
 
-    'logo_mini' => '<b>A</b>LT',
+    'logo_mini' => '<b>IFRO</b>Apps',
 
     /*
     |--------------------------------------------------------------------------
@@ -85,7 +85,7 @@ return [
     |
     */
 
-    'dashboard_url' => 'home',
+    'dashboard_url' => 'admin',
 
     'logout_url' => 'logout',
 
@@ -108,81 +108,69 @@ return [
     */
 
     'menu' => [
-        'MAIN NAVIGATION',
+        // 'MENU',
         [
-            'text' => 'Blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
+            'text'        => 'Home',
+            'url'         => 'admin',
+            'icon'        => 'university',
         ],
+        
         [
-            'text'        => 'Pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        'ACCOUNT SETTINGS',
-        [
-            'text' => 'Profile',
-            'url'  => 'admin/settings',
+            'text' => 'Alunos',
+            'url'  => 'admin',
             'icon' => 'user',
-        ],
-        [
-            'text' => 'Change Password',
-            'url'  => 'admin/settings',
-            'icon' => 'lock',
-        ],
-        [
-            'text'    => 'Multilevel',
-            'icon'    => 'share',
             'submenu' => [
-                [
-                    'text' => 'Level One',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'Level One',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'Level Two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'Level Two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'Level Three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'Level Three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
+                
+                    [
+                        'text' => 'Incluir novo aluno',
+                        'url'  => 'admin/activity-new',
+                        'icon' => 'plus-circle',
+                        
                     ],
-                ],
-                [
-                    'text' => 'Level One',
-                    'url'  => '#',
-                ],
+
+                    [
+                        'text' => 'Listar alunos',
+                        'url'  => 'admin/activities',
+                        'icon' => 'list-ul',
+                    ]
+
+                               
             ],
+
         ],
-        'LABELS',
+
+
+        
+
         [
-            'text'       => 'Important',
-            'icon_color' => 'red',
-        ],
-        [
-            'text'       => 'Warning',
-            'icon_color' => 'yellow',
-        ],
-        [
-            'text'       => 'Information',
-            'icon_color' => 'aqua',
-        ],
+            'text'        => 'Atividades Complementares',
+            'url'         => 'admin',
+            'icon'        => 'graduation-cap',
+            'submenu'     => [
+                [
+                    'text' => 'Atividades',
+                    'url'  => 'admin',
+                    'icon' => 'plus-circle',
+                    'submenu' => [
+                        
+                            [
+                                'text' => 'Incluir nova atividade',
+                                'url'  => 'admin/activity-new',
+                                'icon' => 'plus-circle',
+                            ],
+
+                            [
+                                'text' => 'Listar atividades',
+                                'url'  => 'admin/activities',
+                                'icon' => 'list-ul',
+                            ]
+
+                                       
+                    ],
+
+                ]
+            ]
+        ]
     ],
 
     /*
